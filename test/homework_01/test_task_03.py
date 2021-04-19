89f95367a38067f04d1627a7450f59e9e9879401
Julia <julvain@gmail.com>
2021-04-19 13:22:44 +0300

    Change file paths in test_task_03.py

--- test/homework_01/test_task_03.py
+++ test/homework_01/test_task_03.py
@@ -2,8 +2,8 @@ from homework_01.task_03 import find_maximum_and_minimum
 
 
 def test_1_5():
-    assert find_maximum_and_minimum("test_task03_files/file0.txt") == (1, 5)
+    assert find_maximum_and_minimum("./test_task03_files/file0.txt") == (1, 5)
 
 
 def test_0():
-    assert find_maximum_and_minimum("test_task03_files/file1.txt") == (0, 0)
+    assert find_maximum_and_minimum("./test_task03_files/file1.txt") == (0, 0)
