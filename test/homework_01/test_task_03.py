076f06e8184254c6a2346a155b9e0bcfa3053808
Julia <julvain@gmail.com>
2021-04-19 21:20:38 +0300

    Fix tests' names

--- test/homework_01/test_task_03.py
+++ test/homework_01/test_task_03.py
@@ -1,14 +1,14 @@
 from homework_01.task_03 import find_maximum_and_minimum
 
 
-def test_12345():
+def test_numbers_1_2_3_4_5_return_1_5():
     assert find_maximum_and_minimum("test/homework_01/test_task03_files/file0.txt") == (
         1,
         5,
     )
 
 
-def test_0():
+def test_numbers_0_return_0_0():
     assert find_maximum_and_minimum("test/homework_01/test_task03_files/file1.txt") == (
         0,
         0,
