af594b063bd731b4d4b6d4e187e917b6332c57ed
Julia <julvain@gmail.com>
2021-04-19 12:38:19 +0300

    Optimize task_02.py, correct and add tests

--- test/homework_01/test_task_02.py
+++ test/homework_01/test_task_02.py
@@ -1,13 +1,38 @@
-from homework_01.task_02 import check_fibonacci
+from homework_01.task_02 import check_fibonacci, gen_fibonacci
+
+"""Test gen_fibonacci"""
+
+
+def test_gen_fibonacci_negative_inversed_order_arguments():
+    assert gen_fibonacci(-1, -2) == []
+
+
+def test_gen_fibonacci_0_5():
+    assert gen_fibonacci(0, 5) == [0, 1, 1, 2, 3, 5]
+
+
+def test_gen_fibonacci_0_1():
+    assert gen_fibonacci(0, 1) == [0, 1, 1]
+
+
+def test_gen_fibonacci_1_5():
+    assert gen_fibonacci(1, 2) == [1, 1, 2]
+
+
+def test_gen_fibonacci_1_1():
+    assert gen_fibonacci(1, 1) == [1, 1]
+
+
+"""Test check_fibonacci"""
 
 
 def test_positive_case_from_0():
     """Testing that several consecutive Fibonacci numbers starting with 0 give True"""
-    assert check_fibonacci([0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377])
+    assert check_fibonacci([0, 1, 1, 2, 3, 5])
 
 
 def test_positive_case_not_from_0():
-    """Testing that several consecutive Fibonacci numbers not starting with 0 give True"""
+    """Testing that Fibonacci subsequence not starting with 0 give True"""
     assert check_fibonacci([233, 377])
 
 
@@ -19,6 +44,16 @@ def test_positive_one_number():
 def test_positive_0_1():
     """Testing that [0, 1] gives True"""
     assert check_fibonacci([0, 1])
+
+
+def test_positive_0_1_1():
+    """Testing that [0, 1, 1]"""
+    assert check_fibonacci([0, 1, 1])
+
+
+def test_negative_0_1_2():
+    """Testing that [0, 1, 2] gives False"""
+    assert not check_fibonacci([0, 1, 2])
 
 
 def test_negative_case():
@@ -33,10 +68,4 @@ def test_negative_empty():
 
 def test_negative_case_wrong_order():
     """Testing that a Fibonacci sequence with swapped elements gives False"""
-    assert not check_fibonacci([1, 2, 3, 5, 8, 13, 55, 34, 21, 89, 144])
-
-
-def test_negative_case_wrong_last_el():
-    """Testing that a Fibonacci sequence with two swapped elements including
-    the last one gives False"""
-    assert not check_fibonacci([1, 2, 3, 5, 8, 21, 34, 55, 89, 144, 13])
+    assert not check_fibonacci([13, 55, 34, 21, 89])
