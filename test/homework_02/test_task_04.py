aef96b3b1292e937766d182398af8f15349ffd29
Julia <julvain@gmail.com>
2021-05-18 13:38:24 +0300

    Initial commit

---
+++ test/homework_02/test_task_04.py
@@ -0,0 +1,11 @@
+from homework_02.task_04 import cache
+
+
+def test_func_with_two_args():
+    def func(a, b):
+        return (a ** b) ** 2
+    cache_func = cache(func)
+    some = 100, 200
+    val_1 = cache_func(*some)
+    val_2 = cache_func(*some)
+    assert val_1 is val_2
