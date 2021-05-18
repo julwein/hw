2629293db76e5f3040f4291dcbff1f78dd9871db
Julia <julvain@gmail.com>
2021-05-18 12:54:10 +0300

    Fix formatting

--- test/homework_02/test_task_03.py
+++ test/homework_02/test_task_03.py
@@ -7,8 +7,8 @@ def test_one_input_list():
 
 def test_two_input_lists():
     assert list(combinations([1, 2], [3, 4])) == [
-    [1, 3],
-    [1, 4],
-    [2, 3],
-    [2, 4],
+        [1, 3],
+        [1, 4],
+        [2, 3],
+        [2, 4],
     ]
